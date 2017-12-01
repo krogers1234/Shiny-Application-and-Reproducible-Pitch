ebe8ae8fdf401d9405ea7b4590ccbcbbacfb2aaa
krogers1234 <keith.rogers@cop.com>
2017-12-01 09:56:53 -0600

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,52 @@
+library(shiny)
+
+shinyUI(
+        navbarPage("Shiny App",
+        tabPanel("Analysis",
+        fluidPage(titlePanel("The relationship between vehicle attributes and MPG"),
+        sidebarLayout(sidebarPanel(selectInput("variable", "Variable:",
+               c("Number of cylinders" = "cyl",
+               "Displacement (cu.in.)" = "disp",
+               "HP" = "hp",
+               "Axle ratio" = "drat",
+               "Weight (lb/1000)" = "wt",
+               "1/4 mile time" = "qsec",
+               "Transmission" = "am",
+               "Gears" = "gear"
+                )),
+                                                    
+      checkboxInput("outliers", "outliers", FALSE)
+                                            ),
+                                            
+      mainPanel(h3(textOutput("caption")),
+      tabsetPanel(type = "tabs", 
+      tabPanel("BoxPlot", plotOutput("mpgBoxPlot")),
+      tabPanel("Model", 
+      plotOutput("mpgPlot"),
+      verbatimTextOutput("fit"))))))),
+      tabPanel("Information",
+      hr(),
+      helpText("What vehicle attributes have the greatest impact on MPG"),
+      h3("Scope"),
+      p("32 observations for 8 variables.")),
+      tabPanel("Detail",
+      h2("Motor Trend Car Road Tests"),
+      hr(),
+      h3("Description"),
+      helpText("The data was extracted from the 1974 Motor Trend US magazine,",
+              " and comprised of fuel consumption and 8 aspects of automobile design and performance",
+              " for 32 automobiles (1973-74 models)."),
+      h3("Format"),
+              p("A data frame with 32 observations with 8 variables."),
+              p("  [, 1]         mpg         Miles/(US) gallon"),
+              p("  [, 2]	 cyl	 Number of cylinders"),
+              p("  [, 3]	 disp	 Displacement (cu.in.)"),
+              p("  [, 4]	 hp	 HP"),
+              p("  [, 5]	 drat	 Axle ratio"),
+              p("  [, 6]	 wt	 Weight (lb/1000)"),
+              p("  [, 7]	 qsec	 1/4 mile time"),
+              p("  [, 9]	 am	 Transmission (0 = automatic, 1 = manual)"),
+              p("  [,10]	 gear	 Number of forward gears"),
+              
+              h3("Source"),
+              p("Henderson and Velleman (1981)"))))
